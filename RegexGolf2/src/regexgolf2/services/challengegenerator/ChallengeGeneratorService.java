1ecae340c5dea5d75ea69da8b27a9c246867a1c1
Felix Hennig <3hennig@informatik.uni-hamburg.de>
2014-06-29 01:29:01 +0200

    Added TestGenerator

--- RegexGolf2/src/regexgolf2/services/challengegenerator/ChallengeGeneratorService.java
+++ RegexGolf2/src/regexgolf2/services/challengegenerator/ChallengeGeneratorService.java
@@ -1,15 +1,53 @@
 package regexgolf2.services.challengegenerator;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import regexgolf2.model.Challenge;
 import regexgolf2.model.Solution;
 import regexgolf2.model.SolvableChallenge;
-import regexgolf2.startup.ChallengeFactory;
+import regexgolf2.model.regexgenerator.Generator;
+import regexgolf2.model.regexgenerator.TestGenerator;
+
+import com.google.java.contract.Ensures;
+import com.google.java.contract.Requires;
 
 public class ChallengeGeneratorService
 {
+	private Set<Generator> _generators = new HashSet<>();
+	private Generator _selectedGenerator;
+	
+	
+	
+	public ChallengeGeneratorService()
+	{
+		initGenerators();
+	}
+	
+	
+	
+	private void initGenerators()
+	{
+		Generator g = new TestGenerator();
+		_generators.add(g);
+		_selectedGenerator = g;
+	}
+	
+	@Requires("_generators.contains(generator)")
+	public void setSelectedGenerator(Generator generator)
+	{
+		_selectedGenerator = generator;
+	}
+	
+	@Ensures("result != null")
+	public Generator getSelectedGenerator()
+	{
+		return _selectedGenerator;
+	}
+	
 	public SolvableChallenge generateChallenge()
 	{
-		Challenge c = ChallengeFactory.getRandomChallenge();
+		Challenge c = getSelectedGenerator().generateChallenge();
 		SolvableChallenge sc = new SolvableChallenge(new Solution(), c);
 		
 		return sc;
